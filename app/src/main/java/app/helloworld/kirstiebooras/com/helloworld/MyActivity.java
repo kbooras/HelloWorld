38911dadf7853cfa3a3b016ad1387f9cc23e5df3
kbooras <kbooras@ufl.edu>
2014-08-26 21:55:16 -0400

    added button

--- app/src/main/java/app/helloworld/kirstiebooras/com/helloworld/MyActivity.java
+++ app/src/main/java/app/helloworld/kirstiebooras/com/helloworld/MyActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
@@ -12,6 +15,16 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        final TextView firstTextView = (TextView)findViewById(R.id.textView);
+        Button firstButton = (Button)findViewById(R.id.first_button);
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstTextView.setText("You Clicked!");
+            }
+        });
     }
 
 
